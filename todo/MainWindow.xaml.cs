a02818ba0379a79400ba00551fac75e5c2a4e76e
LAPTOP-77RQU6K6\kangsoup <kingkang004@gmail.com>
2018-06-28 02:39:04 +0800

    不知道如何切換頁面，不完整

---
+++ todo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todo
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int a;
+
+        private void dayBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            a = 1;
+            MainWindow day = new MainWindow ();
+            this.Close();
+            day.Show();
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)//偵測視窗有沒有被關掉
+        {
+            if (a != 1)
+                Application.Current.Shutdown();
+        }
+       
+    }
+}
---
+++ todo/day.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace todo
+{
+    /// <summary>
+    /// day.xaml 的互動邏輯
+    /// </summary>
+    public partial class day : Window
+    {
+        private string nowText;
+
+        private void Textarea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            nowText = Textarea.Text;
+        }
+        public day()
+        {
+            
+        }
+
+        private void dayBtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void addNoteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
